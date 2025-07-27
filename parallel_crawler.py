cbb708afbcb9fdad012ad76d7799f56dbb82c16f
shon <ducsonds2005@gmail.com>
2025-07-27 14:51:32 +0700

    feat: add crawling using StreamingResponse

---
+++ parallel_crawler.py
@@ -0,0 +1,148 @@
+from playwright.async_api import async_playwright, Browser, Playwright
+from urllib.parse import urljoin
+from base_crawler import CrawlSite
+import asyncio
+
+class BachHoaXanhCrawler(CrawlSite):
+    base_url = "https://bachhoaxanh.com"
+    site_name = "BachHoaXanh"
+    def __init__(self):
+        super().__init__(self.base_url, self.site_name)
+        
+    async def crawl_prod_prices(self, prod_name: str, current_browser: Browser):
+        context = await current_browser.new_context()
+        page = await context.new_page()
+        await page.goto(f"{self.base_url}/tim-kiem?key={prod_name}")
+        await page.wait_for_load_state('networkidle')
+        products = await page.locator('xpath=//html/body/div[1]/div/div[2]/div/main/div/div[2]/div').all()
+        print(f"Found {len(products)} products on BachHoaXanh") 
+        return_info = []
+        for i, product in enumerate(products):
+            if i >= 5:
+                break
+            try:
+                p_name = (await product.locator('css=h3').text_content()).strip()
+                price_and_per = (await product.locator('css=div.product_price').text_content()).strip().split('đ')
+                p_price = price_and_per[0].strip()
+                p_per = price_and_per[1].strip()[1:] if len(price_and_per) > 1 else ''
+                p_url_elements = await product.locator('css=a').all()
+                p_url = await p_url_elements[0].get_attribute('href') if len(p_url_elements) > 0 else await product.locator('css=a').get_attribute('href')
+                p_url = (p_url).strip()
+                p_img_elements = await product.locator('css=img').all()
+                p_img = await p_img_elements[0].get_attribute('src') if len(p_img_elements) > 0 else await product.locator('css=img').get_attribute('src')
+                return_info.append({
+                    'name': p_name,
+                    'price': p_price,
+                    'per': p_per,
+                    'url': urljoin(self.base_url, p_url),
+                    'img': p_img,
+                    'source': self.site_name
+                })
+            except Exception as e:
+                print(f"Error processing product {i}: {e}")
+        await page.close()
+        await context.close()
+        if page.is_closed():
+            print(f"Stop crawling from {self.base_url}")
+        return return_info
+
+class WinmartCrawler(CrawlSite):
+    base_url = "https://winmart.vn"
+    site_name = "Winmart"
+    
+    def __init__(self):
+        super().__init__(self.base_url, self.site_name)
+    
+    async def crawl_prod_prices(self, prod_name, current_browser):
+        context = await current_browser.new_context()
+        page = await context.new_page()
+        await page.goto(f"{self.base_url}/search/{prod_name}") 
+        await page.wait_for_load_state('networkidle')
+        products = await page.locator('xpath=/html/body/div[1]/div[1]/div/div[2]/div/div[2]/div[2]/div[2]/div/div').all()
+        print(f"Found {len(products)} products on Winmart")
+        return_info = []
+        for i, product in enumerate(products):
+            if i >= 5:
+                break
+            p_name = (await product.locator('css=div.product-card-two__Title-sc-1lvbgq2-6').inner_text()).strip()
+            p_price = (await product.locator('css=div.product-card-two__Price-sc-1lvbgq2-9').inner_text())[:-2].strip()
+            p_per = (await product.locator('xpath=./div/div[2]').inner_text()).strip()
+            p_url_elements = await product.locator('css=a').all()
+            # print(f"Product URL elements: {len(p_url_elements)}")
+            p_url = await p_url_elements[0].get_attribute('href') if len(p_url_elements) > 0 else await product.locator('css=a').get_attribute('href')
+            p_url = (p_url).strip()
+            p_img = (await product.locator('css=img.product-image').get_attribute('src')).strip()
+            return_info.append({
+                'name': p_name,
+                'price': p_price,
+                'per': p_per,
+                'url': urljoin(self.base_url, p_url),
+                'img': p_img,
+                'source': self.site_name
+            })
+        await page.close()
+        await context.close()
+        if page.is_closed():
+            print(f"Stop crawling from {self.base_url}")
+        return return_info
+
+class CoopOnlineCrawler(CrawlSite):
+    base_url = "https://cooponline.vn"
+    site_name = "Co.op Online"
+    
+    def __init__(self):
+        super().__init__(self.base_url, self.site_name)
+    
+    async def crawl_prod_prices(self, prod_name, current_browser):
+        context = await current_browser.new_context()
+        page = await context.new_page()
+        await page.goto(f"{self.base_url}/search?router=productListing&query={prod_name}") 
+        await page.wait_for_load_state('networkidle')
+        products = await page.locator('css=div.css-1y2krk0 div.css-13w7uog').all()
+        print(f"Found {len(products)} products on Coop Online")
+        return_info = []
+        for i, product in enumerate(products):
+            if i >= 5:
+                break
+            p_name = (await product.locator('css=h3.css-1xdyrhj').inner_text()).strip()
+            p_price = (await product.locator('css=div.att-product-detail-latest-price').inner_text())[:-2].strip()
+            p_per = (await product.locator('css=div.css-1f5a6jh').inner_text())[13:].strip()
+            p_url_elements = await product.locator('css=a').all()
+            p_url = await p_url_elements[0].get_attribute('href') if len(p_url_elements) > 0 else await product.locator('css=a').get_attribute('href')
+            p_url = (p_url).strip()
+            p_img = (await product.locator('css=img').get_attribute('src')).strip()
+            return_info.append({
+                'name': p_name,
+                'price': p_price,
+                'per': p_per,
+                'url': urljoin(self.base_url, p_url),
+                'img': p_img,
+                'source': self.site_name
+            }) 
+        await page.close()
+        await context.close()
+        if page.is_closed():
+            print(f"Stop crawling from {self.base_url}")
+        return return_info
+
+async def crawl_prod(prod_name: str):
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        crawl_sites = [BachHoaXanhCrawler(), WinmartCrawler(), CoopOnlineCrawler()]
+        # Tasks for each crawler
+        tasks = [site.crawl_prod_prices(prod_name, browser) for site in crawl_sites]
+        
+        # Gather results from all crawlers
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+        return_info = []
+        for result in results:
+            if isinstance(result, Exception):
+                print(f"Error occurred: {result}")
+            else:
+                return_info.extend(result)
+        await browser.close()
+        print("Browser closed, finished crawling.")
+        return return_info    
+        
+        
+# asyncio.run(crawl_prod('trà sữa trân châu'))
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
-from main import crawl_prod
+from streaming_crawl import streaming_crawl_prod
+from parallel_crawler import crawl_prod
+# import asyncio
+import json
 app = FastAPI()
 
 @app.get('/crawl')
@@ -9,10 +12,49 @@ async def crawl_controller(name: str = ""):
         return {"error": True, "message": "Product name is required."}
     print(f"Received request to crawl product: {name}")
     try:
-        # products = [product async for product in crawl_prod(name)]
         products = await crawl_prod(name)
         if not products:
             return {"error": True, "message": "No products found."}
         return {"error": False, "message": "Data crawled successfully.", "data": products}
     except Exception as e:    
         return {"error": True, "message": str(e)}
+
+
+# Streaming response for incremental data generation
+async def generate_streaming_crawl_prod(name: str):
+    yield '['
+    first_item = True
+    async for product in streaming_crawl_prod(name):
+        if first_item:
+            first_item = False
+            yield json.dumps(product, ensure_ascii=False)
+        yield "," + json.dumps(product, ensure_ascii=False)
+    # Remove the last comma and close the JSON array
+    yield ']'
+
+@app.get('/streaming-crawl')
+async def streaming_crawl_controller(name: str = ""):
+    if not name:
+        return {"error": True, "message": "Product name is required."}
+    print(f"Received request to streaming-crawl product: {name}")
+    try:
+        return StreamingResponse(generate_streaming_crawl_prod(name), media_type='application/json')
+    except Exception as e:    
+        return {"error": True, "message": str(e)}
+    
+    
+# Example of incrementally generating NDJSON data with StreamingResponse
+# only for testing purposes
+# async def generate_ndjson():
+#     yield '['
+#     for i in range(1000):
+#         item = {"index": i}
+#         yield json.dumps(item) 
+#         if i < 999:
+#             yield ','
+#         await asyncio.sleep(0.01)  # simulate delay
+#     yield ']'
+    
+# @app.get('/stream-ndjson')
+# async def stream_ndjson_controller():
+#     return StreamingResponse(generate_ndjson(), media_type='application/json') 
